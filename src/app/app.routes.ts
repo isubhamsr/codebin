a95f3c5aa5585871767f93a97d7036047b668a10
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2024-07-21 05:31:56 +0530

    add lazy loading, reacttive form, template driven form, routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./components/about/about.component').then(
+        (m) => m.AboutComponent
+      ),
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PagenotfoundComponent },
+];
